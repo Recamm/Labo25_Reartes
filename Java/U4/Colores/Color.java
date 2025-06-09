ee3783f69fcb2d3e8f7ed78fe08e62888da82fc8
Recamm <camiloreartesgm@gmail.com>
2025-06-09 09:07:33 -0300

    Color enums

--- Java/U4/Colores/Color.java
+++ Java/U4/Colores/Color.java
@@ -1,11 +1,11 @@
 package U4.Colores;
 
-public enum Colores {
+public enum Color {
     ROJO("ff0000"), AZUL("0087ff"), VERDE("00ff23"), AMARILLO("fbff00");
 
     private String colorHexa;
 
-    private Colores (String colorHexa){
+    private Color(String colorHexa){
         this.colorHexa = colorHexa;
     }
 }
